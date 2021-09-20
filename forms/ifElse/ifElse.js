0e012c854bb6d713daa4a8db2110804eab8f70cf
KhaiDinhVan <52925334+KhaiDinhVan@users.noreply.github.com>
2021-09-19 22:34:34 -0500

    add comments

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -1,28 +1,34 @@
-// Get user input for user first name, states (2 letters), user's temperature in Fahrenheit 
+// Get user input for user first name, states (2 letters), and user's temperature in Fahrenheit 
 
 let firstName = prompt("what is your first name?")
 let userState = prompt('what is your states (2 letters)?')
 let userTemp = Number(prompt('what is the temperature in your state? (Fahrenheit)'))
 
-// store the message in 
+// store each message in an array, which is a collection of element, in this case is the message that we will output to user
 const messages = [];
-messages[0] = "Wear a warm coat, hat, scarf and gloves.";
-messages[1] = "Wear a warm coat but you won't need a hat, scarf or gloves.";
-messages[2] = "Wear your warmest coat, a warm hat, a scarf, and warm gloves.";
-messages[3] = "Wear a warm coat, hat and gloves. Maybe a scarf too."
+messages[0] = "wear a warm coat, hat, scarf and gloves.";
+messages[1] = "wear a warm coat but you won't need a hat, scarf or gloves.";
+messages[2] = "wear your warmest coat, a warm hat, a scarf, and warm gloves.";
+messages[3] = "wear a warm coat, hat and gloves. Maybe a scarf too."
 
+// Take input the tempurature and the state from user compare in each case to give the right output
+// If the tempurature is less than 32 and the state the user input is NE, then tell user the first message
 if (userTemp < 32 && userState == "NE") {
-  console.log(messages[0])
+  console.log(`${firstName}, ${messages[0]}`)
 }
+// If the tempurature is greater than or equal to 32 and less than or equal to 50 and the state input is NE, then tell user the second message
 else if (userTemp >= 32 && userTemp <= 50  && userState == "NE") {
-  console.log(messages[1])
+  console.log(`${firstName}, ${messages[1]}`)
 }
+// If the tempurature is greater than or equal to 32 and less than or equal to 50 and the state input is FL, then tell user the third message
 else if (userTemp >= 32 && userTemp <= 50  && userState == "FL") {
-  console.log(messages[2])
+  console.log(`${firstName}, ${messages[2]}`)
 }
+// If the tempurature is greater than or equal to 50 and less than or equal to 70 and the state input is FL, then tell user the fourth message
 else if (userTemp >= 50 && userTemp <= 70  && userState == "FL") {
-  console.log(messages[3])
+  console.log(`${firstName}, ${messages[3]}`)
 }
+// If none of the case above apply, then tell the user to try again 
 else {
   console.log('You should try again')
 }
--- forms/switch/switch.js
+++ forms/switch/switch.js
@@ -4,27 +4,33 @@ let firstName = prompt("what is your first name?")
 let userState = prompt("what is your states (2 letters)?")
 let userTemp = Number(prompt("what is the temperature in your state? (Fahrenheit)"))
 
-// Store messages so we could use it later. khai
+// Store messages in the array so we could use it later.
+//an array, which is a collection of element, in this case is the message that we will output to user
 const messages = [];
-messages[0] = "Wear a warm coat, hat, scarf and gloves.";
-messages[1] = "Wear a warm coat but you won't need a hat, scarf or gloves.";
-messages[2] = "Wear your warmest coat, a warm hat, a scarf, and warm gloves.";
-messages[3] = "Wear a warm coat, hat and gloves. Maybe a scarf too."
+messages[0] = "wear a warm coat, hat, scarf and gloves.";
+messages[1] = "wear a warm coat but you won't need a hat, scarf or gloves.";
+messages[2] = "wear your warmest coat, a warm hat, a scarf, and warm gloves.";
+messages[3] = "wear a warm coat, hat and gloves. Maybe a scarf too."
 
-// If the user does input, then the code runs from each of the tempurature and states
+// If the user does input, then the code runs for each case of the tempurature and states. 
+// If the tempurature is less than 32 and the state the user input is NE, then tell user the first message
 switch (true) {
 case (userTemp < 32 && userState == "NE"):
-  console.log(messages[0])
+  console.log(`${firstName}, ${messages[0]}`)
   break
+// If the tempurature is greater than or equal to 32 and less than or equal to 50 and the state input is NE, then tell user the second message
 case (userTemp >= 32 && userTemp <= 50  && userState == "NE"):
-  console.log(messages[1])
+  console.log(`${firstName}, ${messages[1]}`)
   break
+// If the tempurature is greater than or equal to 32 and less than or equal to 50 and the state input is FL, then tell user the third message
 case (userTemp >= 32 && userTemp <= 50  && userState == "FL"):
-  console.log(messages[2])
+  console.log(`${firstName}, ${messages[2]}`)
   break
+// If the tempurature is greater than or equal to 50 and less than or equal to 70 and the state input is FL, then tell user the fourth message
 case (userTemp >= 50 && userTemp <= 70  && userState == "FL"):
-  console.log(messages[3])
+  console.log(`${firstName}, ${messages[3]}`)
   break
+// If none of the case above apply, then tell the user to try again 
 default:
   console.log("Please try again.")
 }
